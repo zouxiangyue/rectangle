c6b6adbe7e972545a4cc8b9c64610d4fe3060262
zouxiangyue <1539442166@qq.com>
2019-03-27 16:46:48 +0800

    计算器v1.0 字段校验

--- calc.js
+++ calc.js
@@ -12,3 +12,24 @@ function Rectangle(width, height) {
     };
 
 }
+
+function valid(value){
+  var result={
+    isOk:false,
+    reason:''
+  }
+  if(value===''){
+    result.reason='不能为空！';
+    return result;
+  }else if(value<=0){
+    result.reason='不能为非正数字！';
+    return result;
+  }else if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(value)){
+    result.reason="不能为非法数字！";
+    return result;
+  }else{
+    result.isOk=true;
+    return result;
+  }
+
+}
--- rectangle.js
+++ rectangle.js
@@ -4,12 +4,38 @@ $(function() {
   var $btnCal = $('#calculate');
   var $perimeter = $('#perimeter');
   var    $area = $('#area');
+  var    $widthValidation=$('#width-validation');
+  var    $heightValidation=$('#height-validation');
 
+  $width.blur(()=>{
+    var w=$width.val();
+    var result= valid(w);
+    if(!result.isOk){
+      $widthValidation.html('宽度'+result.reason);
+      $width,focus();
+    }else{
+      $widthValidation.html('')
+    }
+  });
+  $height.blur(()=>{
+    var h=$height.val();
+    var result=valid(h);
+    if(!result.isOk){
+      $heightValidation.html('高度'+result.reason);
+      $height.focus();
+    }else{
+      $heightValidation.html('');
+    }
+  })
   $btnCal.click(function(){
     var w = $width.val();
     var h = $height.val();
+    if(!valid(w).isOk || !valid(h).isOk){
+      $perimeter.val('');
+      $area.val('')
+      return;
+    }
     var r = new Rectangle(w, h);
-
     $perimeter.val(r.perimeter());
     $area.val(r.area());
               
